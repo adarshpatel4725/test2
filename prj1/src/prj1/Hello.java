e8953e92fc0ca62798fde10c72d234740558f4fd
Adarsh patel <adarshpatel4725@gmail.com>
2017-06-13 17:02:49 +0530

    third

---
+++ prj1/src/prj1/Hello.java
@@ -0,0 +1,19 @@
+
+package prj1;
+
+/**
+ * @author axelor
+ *
+ */
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("hello,world");
+		
+	}
+
+}
